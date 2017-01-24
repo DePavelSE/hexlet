247b013c990926809375ed63f70fc32a45d2aa0c
pavel-else <de.pavel.se@yandex.ru>
2017-01-24 22:42:41 +0300

    mutators

--- mutators.js
+++ mutators.js
@@ -1,3 +1,24 @@
-export default (num) => {
-	return num;
+/*
+Простое число — натуральное (целое положительное) число (больше единицы),
+имеющее ровно два различных натуральных делителя — единицу и самого себя.
+
+solution.js
+Реализуйте и экспортируйте функцию isPrime определяющую, является ли число простым.
+
+Пример:
+
+isPrime(1); // false
+isPrime(7); // true
+isPrime(10); // false
+*/
+
+export const isPrime = (num) => {
+	if (num <= 1 || typeof(num) != 'number') return false;
+
+	let i = 2;
+	while (i < num) {
+		if (num % i === 0) return false;
+		i++;
+	}
+	return true;
 }
